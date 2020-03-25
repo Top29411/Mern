1f6c72c4f9062bb010505c17f027a11337a84454
Topdev2022 <sergebachkin@gmail.com>
2020-03-25 13:49:44 +0100

    move middlewares

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ import usersRouter from './routes/users';
 
 const app = express();
 
-var limiter = new rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // limit each IP to 50 requests per windowMs
-  delayMs: 0 // disable delaying - full speed until the max limit is reached
-});
-
 app.disable('x-powered-by');
 
 app.use(compression());
@@ -27,9 +21,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(favicon(__dirname + '/favicon.ico'));
-app.use(helmet());
-app.use(limiter);
+app.use(helmet()); // app.disable('x-powered-by');
 app.use(morgan('dev'));
+app.use(
+  new rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50, // limit each IP to 50 requests per windowMs
+    delayMs: 0 // disable delaying - full speed until the max limit is reached
+  })
+);
 app.use(responseTime());
 
 app.use('/', indexRouter);
