43e2fab44820f969a4eda263f54ab51f0de1b38d
Topdev2022 <sergebachkin@gmail.com>
2020-03-25 19:45:17 +0100

    modify messsage

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(
   new rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 50, // limit each IP to 50 requests per windowMs
-    message: 'Too many accounts from this IP, please try again in 15 minutes'
+    message: 'Too many requests from this IP, please try again in 15 minutes'
   })
 );
 
